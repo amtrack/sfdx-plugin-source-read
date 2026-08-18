2fd41a794958e4adb502d3bfade94cd8c5b1deb1
Matthias Rolke <mr.amtrack@gmail.com>
2026-08-18 11:10:30 +0200

    chore: add format script

--- test/e2e.ts
+++ test/e2e.ts
@@ -2,5 +2,8 @@ import { execa, parseCommandString } from "execa";
 import { resolve } from "node:path";
 
 export async function run(pluginCommand) {
-  return await execa(resolve("bin", "run.js"), parseCommandString(pluginCommand));
+  return await execa(
+    resolve("bin", "run.js"),
+    parseCommandString(pluginCommand)
+  );
 }
